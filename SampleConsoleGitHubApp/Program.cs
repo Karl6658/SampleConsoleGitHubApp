c1913201c1ba2c5f78e107f89136d6816cbef2d5
Karl Burke <karl6658@yahoo.co.uk>
2024-05-06 12:14:21 +0100

    Added Trace

--- SampleConsoleGitHubApp/Program.cs
+++ SampleConsoleGitHubApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace SampleConsoleGitHubApp
     {
         static void Main(string[] args)
         {
+            Trace.WriteLine("Console App Started");
         }
     }
 }
